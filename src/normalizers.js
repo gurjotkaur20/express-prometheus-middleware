761646d3354c42e7b151dc29d6f653305feb5c2e
zachery2008 <zachery.zhu@lifebooster.ca>
2021-05-11 23:15:28 -0700

    v1.0.7

--- src/normalizers.js
+++ src/normalizers.js
@@ -1,46 +1,34 @@
-const url = require('url');
 const UrlValueParser = require('url-value-parser');
+const url = require('url');
 
-/**
- * Normalizes urls paths.
- *
- * This function replaces route params like ids, with a placeholder, so we can
- * set the metrics label, correctly. E.g., both routes
- *
- * - /api/v1/user/1
- * - /api/v1/user/2
- *
- * represents the same logical route, and we want to group them together,
- * hence the need for the normalization.
- */
-function normalizePath(req, options) {
-  // ATTENTION! urlValueParser is a singleton!
-  let urlValueParser;
+// ATTENTION! urlValueParser is a singleton!
+let urlValueParser;
+
+function normalizePath(req, opts) {
   // originalUrl is taken, because url and path can be changed
   // by middlewares such as 'router'. Note: this function is called onFinish
   /// i.e. always in the tail of the middleware chain
   let path = url.parse(req.originalUrl || req.url).pathname;
 
-  const normalizePath = options && options.normalizePath;
+  const normalizePath = opts && opts.normalizePath;
   if (Array.isArray(normalizePath)) {
     for (const tuple of normalizePath) {
       if (!Array.isArray(tuple) || tuple.length !== 2) {
         throw new Error('Bad tuple provided in normalizePath option, expected: [regex, replacement]');
       }
       const regex = typeof tuple[0] === 'string' ? RegExp(tuple[0]) : tuple[0];
-      if(regex.test(path)) {
+      if (regex.test(path)) {
         path = path.replace(regex, tuple[1]);
-        break;
+        return path;
       }
+      
     }
   }
-  /*
+
   if (!urlValueParser) {
-    urlValueParser = new UrlValueParser(options && options.urlValueParser);
+    urlValueParser = new UrlValueParser(opts && opts.urlValueParser);
   }
   return urlValueParser.replacePathValues(path);
-  */
- return path;
 };
 
 /**
